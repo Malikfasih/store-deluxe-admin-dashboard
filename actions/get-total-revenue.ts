c6e7bc50d5478f0989aa24ca863df2082bdf4929
Malikfasih <90088021+Malikfasih@users.noreply.github.com>
2023-08-19 16:19:49 +0500

    initial commit

---
+++ actions/get-total-revenue.ts
@@ -0,0 +1,28 @@
+import prismadb from "@/lib/prismadb";
+
+export const getTotalRevenue = async (storeId: string) => {
+  const paidOrders = await prismadb.order.findMany({
+    where: {
+      storeId,
+      isPaid: true,
+    },
+    include: {
+      orderItems: {
+        include: {
+          product: true,
+        },
+      },
+    },
+  });
+
+  //   iterate through all orders
+  const totalRevenue = paidOrders.reduce((total, order) => {
+    // iterate through all order-items
+    const orderTotal = order.orderItems.reduce((orderSum, item) => {
+      return orderSum + item.product.price.toNumber();
+    }, 0);
+    return total + orderTotal;
+  }, 0);
+
+  return totalRevenue;
+};
